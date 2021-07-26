529dbdc95c335dc26f2b1dbc3a2cb732c305ae7e
Chathuranga <nchathu2014@gmail.com>
2021-07-27 01:49:05 +0530

    PUT request, DRY approach to make a single Joi function for validation

--- index.js
+++ index.js
@@ -33,19 +33,18 @@ app.get('/api/courses/:id', (req, res) => {
 
 app.post('/api/courses', (req, res) => {
 
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
 
-    const result = Joi.validate(req.body, schema);
-    if (result.error) {
-        //Bad Request
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
+    }
+
+    const { error } = validateCourse(course);
+    if (!error) {
+        //Bad Request
+        res.status(400).send(result.error.details[0].message);
+        return;
     }
 
     courses.push(course);
@@ -53,6 +52,37 @@ app.post('/api/courses', (req, res) => {
 });
 
 
+app.put('/api/courses/:id', (req, res) => {
+    const courseId = req.params.id;
+    const courseName = req.body.name;
+
+    const course = _.find(courses, function (course) {
+        return course.id === parseInt(courseId)
+    })
+
+    const { error } = validateCourse(course);
+
+    if (!course) res.status(404).send('Course Not Found :(');
+    if (!error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    course.name = courseName;
+    res.send(course);
+
+})
+
+
 //environment variables
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening...to port ${port}`));
+
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    const result = Joi.validate(course, schema);
+
+    return result;
+}
