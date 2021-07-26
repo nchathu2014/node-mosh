9396b14acbfb6d5b5177f7d704d1654560e841b2
Chathuranga <nchathu2014@gmail.com>
2021-07-26 22:27:13 +0530

    route params and query params, env parameters, nodemon

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # node-mosh
+
 Learning NodeJS with Mosh
+
+# npm commands
+
+## npm list --depth=0
+
+## npm view <module-name> version | dependencies
+
+## npm outdated | npm updated
+
+## npm-check-updated
--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const _ = require('underscore');
-const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
-const results = _.contains([1, 2, 3], 3);
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!!!.....</h1>');
+});
 
-console.log(results);
+//Route parameters
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3, 100, 1000]);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(id);
+});
+
+app.get('/api/courses/:id/price/:price', (req, res) => {
+    const id = req.params.id;
+    const price = req.params.price;
+    res.send({ id, price });
+});
+
+//query parameters
+app.get('/api/customers/:id', (req, res) => {
+    const queryParam = req.query;
+    res.send(queryParam);
+});
+
+
+//environment variables
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening... to port ${port}`));
